a49c779e63adfb8bf0a841957858031c98fbb500
Amoz <amoz.pay@epitech.eu>
2021-07-24 15:05:31 +0200

    fix: more react events regex, event lower cased

--- src/parser/reactEvents.py
+++ src/parser/reactEvents.py
@@ -2,13 +2,13 @@ import regex
 from reactTypes import *
 
 REGEXP = {
-    "onEvent": regex.compile(r'(\s)(on)(.*=)')
+    "onEvent": regex.compile(r'(\<[^><]+)(on)([A-Z][^=]*)=(\{(?:[^{}]*|(?4))*\})[^<>]*>')
 }
 
 def parseReactEvents(html):
     find = REGEXP["onEvent"].findall(html)
     for element in find:
         begin = element[0] + element[1] + element[2]
-        end = element[0] + element[1] + ":" + element[2]
+        end = element[0] + element[1] + ":" + element[2].lower()
         html = regex.sub(begin, end, html)
     return html
