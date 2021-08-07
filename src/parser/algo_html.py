5167a4d3c413134e179d16d3fc8594f4f464838e
Baptiste Barbotin <baptiste.barbotin@epitech.eu>
2021-08-07 10:56:15 +0200

    fix quote and curly braquets bug and add loop transpilation

--- src/parser/algo_html.py
+++ src/parser/algo_html.py
@@ -13,26 +13,55 @@ def get_car_pos(html_content, car):
 
     for letter in html_content:
         if (letter == car):
-            break
+            return compt
         compt = compt + 1
-    return compt
+    return -1
 
 def get_html_line(content, html_line):
     pos = html_line.span()
     html_content = content[pos[0] + 6:]
-    car_pos = get_car_pos(html_content, '\n')
+    car_pos = get_car_pos(html_content, ';')
+    if (car_pos == -1):
+        car_pos = get_car_pos(html_content, '\n')
+    if (car_pos == -1):
+        car_pos = get_car_pos(html_content, '}')
     html_content = html_content[:car_pos]
-    print(html_content)
-    print("\n\n")
     return html_content
+
+def check_quote(letter, quoteCheck):
+    if (letter == '"' and quoteCheck == False):
+        quoteCheck = True
+    elif (letter == '"' and quoteCheck == True):
+        quoteCheck = False
+    return letter, quoteCheck
+
+def checkcurlBracket(letter, curlBracketCheck, checkExeption):
+    if (letter == '{'):
+        curlBracketCheck = curlBracketCheck + 1
+    if (letter == '}'):
+        curlBracketCheck = curlBracketCheck - 1
+    if (curlBracketCheck <= 0):
+        curlBracketCheck = 0
+        checkExeption = False
+    else:
+        checkExeption = True
+    return letter, curlBracketCheck, checkExeption
 
 def get_end_block(html_content):
     begin = [0, False]
     end = [0, False]
     pos = 0
     compt = 0
+    checkExeption = [False, False]
+    curlBracketCheck = 0
 
     for letter in html_content:
+        letter, checkExeption[0] = check_quote(letter, checkExeption[0])
+        if (checkExeption[0] != True):
+            letter, curlBracketCheck, checkExeption[1] = checkcurlBracket(letter, curlBracketCheck, checkExeption[1])
+        if (checkExeption[0] == True or checkExeption[1] == True):
+            pos = pos + 1
+            continue
         if (letter == '(' and begin[1] == False):
             begin[0] = pos + 1
             begin[1] = True
@@ -54,37 +83,15 @@ def get_html_block(content, html_block):
     car_pos = get_car_pos(html_content, '(')
     html_content = html_content[car_pos:]
     html_content = get_end_block(html_content)
-    print(html_content)
-    print("\n\n")
-    return
+    return html_content
 
 
 
 def html_finder(content):
     html_line = REGEXP['html line'].search(content)
     html_block = REGEXP['html block'].search(content)
-
+    
     if (html_line):
         return get_html_line(content, html_line)
     if (html_block):
         return get_html_block(content, html_block)
-        
-        
-    # if (html_block):
-    #     print("html block")
-    #     print("\n\n")
-    #     return
-    
-    # pos = html.span();
-    # html_content = content[pos[0]:]
-
-    # compt = 0
-    # for letter in html_content:
-    #     if (letter == '(' and compt != 0):
-    #         compt += 1
-    #     if (letter == ')'):
-    #         compt -= 1
-    # print(compt)
-    # print(html_content)
-    # print("\n\n")
-    # return
--- src/parser/loops.py
+++ src/parser/loops.py
@@ -1,9 +1,11 @@
 import regex
+from regex.regex import Regex
 from reactTypes import *
 from parser.useRegex import useRegex
 
 REGEXP = {
-    "loop": regex.compile(r'\{.*map.*=>.*\}')
+    "loop": regex.compile(r'\{(.*)\.map.*=>.*(\<.*\>).*\}'),
+    "var": regex.compile(r'=\{(.*)\}')
 }
 
 def parseProps(html, variables):
@@ -12,3 +14,12 @@ def parseProps(html, variables):
         html = regex.sub("props.", "", html)
         variables.append(Variable("export let", props[i], "undefined"))
     return html, variables
+
+def parseLoop(html):
+    find = REGEXP["loop"].findall(html)
+
+    if (find):
+        findVar = regex.sub(REGEXP["var"], "={element}", find[0][1])
+        result = "{#each " + find[0][0] + " as element}\n" + findVar + "\n{/each}"
+        html = regex.sub(REGEXP["loop"], result, html)
+    return html
